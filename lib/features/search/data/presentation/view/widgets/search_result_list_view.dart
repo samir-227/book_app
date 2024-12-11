e60813419cf46a146d746663e9fd550a1988f4ff
samir-227 <samermedhat227@gmail.com>
2024-12-11 19:17:43 +0200

    create search result list view

---
+++ lib/features/search/data/presentation/view/widgets/search_result_list_view.dart
@@ -0,0 +1,20 @@
+import 'package:bookly_app/features/home/presentation/views/widgets/custom_best_seller_item.dart';
+import 'package:flutter/material.dart';
+
+class ResultSearchListView extends StatelessWidget {
+  const ResultSearchListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.all(0),
+      itemBuilder: (context, index) {
+        return const Padding(
+          padding: EdgeInsets.only(bottom: 16),
+          child: BestSellerListViewItem(),
+        );
+      },
+      itemCount: 10,
+    );
+  }
+}
