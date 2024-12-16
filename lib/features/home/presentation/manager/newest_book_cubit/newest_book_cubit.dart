100c982651dc796226903179eb7c6219b65fe6b5
samir-227 <samermedhat227@gmail.com>
2024-12-16 13:38:26 +0200

    create newest book cubit done

--- lib/features/home/presentation/manager/newest_book_cubit/newest_book_cubit.dart
+++ lib/features/home/presentation/manager/newest_book_cubit/newest_book_cubit.dart
@@ -1,9 +1,18 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/features/home/data/model/book_model/book_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo_imp.dart';
 import 'package:equatable/equatable.dart';
 
 part 'newest_book_state.dart';
 
 class NewestBookCubit extends Cubit<NewestBookState> {
-  NewestBookCubit() : super(NewestBookInitial());
+  NewestBookCubit(this.homeRepoImplement) : super(NewestBookInitial());
+  final HomeRepoImplement homeRepoImplement;
+
+  Future<void> FetchNewestBook() async {
+    emit(NewestBookLoading());
+    var result = await homeRepoImplement.FetchNewestBooks();
+    return result.fold((failure) => emit(NewestBookFailure(failure.errMessage)),
+        (books) => emit(NewestBookSuccess(books)));
+  }
 }
